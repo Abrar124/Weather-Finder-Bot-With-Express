7b97e24a4846940413a8c8c5651d6d3f5ce3ed8f
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-07 20:04:43 +0500

    implement weather API

--- server.js
+++ server.js
@@ -2,32 +2,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var request = require("request");
 
-const apiKey = "4970e4f266675063af77ad454f45ebd6";
-const cityName = 'karachi';
+// const cityName = 'karachi';
 
-url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
-request(url, function (err, response, body) {
-  if(err)
-    { console.log('error:', error);
-   } else {
-     let weather = JSON.parse(body)
-     let message = `It's ${weather.main.temp} degrees and ${weather.main.humidity} in ${weather.name}!`;
-     console.log(message);}
- });
+// request(url, function (err, response, body) {
+//   if(err)
+//     { console.log('error:', error);
+//    } else {
+//      let weather = JSON.parse(body)
+//      let message = `It's ${weather.main.temp} degrees and ${weather.main.humidity} in ${weather.name}!`;
+//      console.log(message);}
+//  });
 
 const { WebhookClient } = require("dialogflow-fulfillment");
 
 const expressApp = express().use(bodyParser.json());
 
 expressApp.post("/webhook", function(req, res, next) {
+  const agent = new WebhookClient({ request, response });
   const gapp = dialogflow({
     req: req,
     res: res
   });
 
   gapp.intent("Find weather", conv => {
-   
-     conv.add(`The current weather in the ${cityName}  is `);
+    const apiKey = "4970e4f266675063af77ad454f45ebd6";
+    const cityName = agent.parameters.name;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+    request(url, function(err, response, body) {
+      let weather = JSON.parse(body);
+
+      console.log(cityName);
+      conv.add(
+        `The current weather in the ${cityName}  is ${weather.main.temp} and ${
+          weather.main.humidity
+        } humidity `
+      );
+    });
   });
 
   gapp.intent("Default Welcome Intent", conv => {

f3b7bbedbf97d8d9ca9b18788645e690ec541291
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-07 19:42:29 +0500

    Basic expess routes with heroku

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var request = require("request");
 
-const { dialogflow, Image } = require("actions-on-google");
+const apiKey = "4970e4f266675063af77ad454f45ebd6";
+const cityName = 'karachi';
 
-const gapp = dialogflow();
-gapp.intent("Default Welcome Intent", conv => {
-  conv.add("Hi, I will help you to find weather");
-});
+url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+request(url, function (err, response, body) {
+  if(err)
+    { console.log('error:', error);
+   } else {
+     let weather = JSON.parse(body)
+     let message = `It's ${weather.main.temp} degrees and ${weather.main.humidity} in ${weather.name}!`;
+     console.log(message);}
+ });
 
-
-gapp.intent("Default Fallback Intent", conv => {
-  conv.add(`I didn't understand. Can you tell me something else?`);
-});
+const { WebhookClient } = require("dialogflow-fulfillment");
 
 const expressApp = express().use(bodyParser.json());
-expressApp.post("/webhook", gapp);
 
+expressApp.post("/webhook", function(req, res, next) {
+  const gapp = dialogflow({
+    req: req,
+    res: res
+  });
 
-expressApp.get('/', function (req, res) {
-  res.send('hello world');
+  gapp.intent("Find weather", conv => {
+   
+     conv.add(`The current weather in the ${cityName}  is `);
+  });
+
+  gapp.intent("Default Welcome Intent", conv => {
+    conv.add("Hi, I will help you to find weather");
+  });
+
+  gapp.intent("Default Fallback Intent", conv => {
+    conv.add(`I didn't understand. Can you tell me something else?`);
+  });
 });
+
+// expressApp.get("/", function(req, res) {
+//   res.send("hello world");
+// });
 
 expressApp.listen(process.env.PORT || 3000, function() {
   console.log("app is running in 3000");

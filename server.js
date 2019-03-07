dc2ccac4c0a423b3f267314daa7bbafe9458a7a2
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-07 17:27:00 +0500

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const { dialogflow, Image } = require("actions-on-google");
 
 const gapp = dialogflow();
-
 gapp.intent("Default Welcome Intent", conv => {
   conv.add("Hi, I will help you to find weather");
 });
@@ -16,6 +15,12 @@ gapp.intent("Default Fallback Intent", conv => {
 
 const expressApp = express().use(bodyParser.json());
 expressApp.post("/webhook", gapp);
+
+
+expressApp.get('/', function (req, res) {
+  res.send('hello world');
+});
+
 expressApp.listen(process.env.PORT || 3000, function() {
   console.log("app is running in 3000");
 });

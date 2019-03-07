179345647d063d89c7116024aae871a98cb03dc5
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-07 21:26:14 +0500

    make it better

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const expressApp = express().use(bodyParser.json());
 
 expressApp.post("/webhook", function(request, response, next) {
   // const agent = new WebhookClient({ request, response });
-  const gapp = dialogflow({
-    request: request,
-    response: response,
-  });
+  const gapp = new WebhookClient({ request: request, response: response });
+  // const gapp = dialogflow({
+  //   request: request,
+  //   response: response
+  // });
 
   gapp.intent("Find weather", conv => {
     const apiKey = "4970e4f266675063af77ad454f45ebd6";
-    const cityName = agent.parameters.name;
+    const cityName = gapp.parameters.name;
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
     request(url, function(err, response, body) {
       let weather = JSON.parse(body);

4251d69dc3d8dcd2b583c5cabee30305574ae9c1
Abrar124 <abrar.khurshid.124@gmail.com>
2019-03-14 14:54:28 +0500

    solve error

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ expressApp.post("/webhook", function (request, response, next) {
     //     console.log(message);
     //   }
     // });
-    const temperature = url.main.temp;
+    let weather = JSON.parse(body)
+    const temperature = weather.main.temp;
     
 
     agent.add(`The weather for the city ${cityName} is: ${temperature} degree  `);
@@ -72,3 +73,40 @@ expressApp.listen(process.env.PORT || 3000, function () {
 //   res.send("hello world");
 // });
 
+// function callWeatherApi (city, date) {
+//   return new Promise((resolve, reject) => {
+//     // Create the path for the HTTP request to get the weather
+//     let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+//       '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+//     console.log('API Request: ' + host + path);
+
+//     // Make the HTTP request to get the weather
+//     http.get({host: host, path: path}, (res) => {
+//       let body = ''; // var to store the response chunks
+//       res.on('data', (d) => { body += d; }); // store each response chunk
+//       res.on('end', () => {
+//         // After all the data has been received parse the JSON for desired data
+//         let response = JSON.parse(body);
+//         let forecast = response['data']['weather'][0];
+//         let location = response['data']['request'][0];
+//         let conditions = response['data']['current_condition'][0];
+//         let currentConditions = conditions['weatherDesc'][0]['value'];
+
+//         // Create response
+//         let output = `Current conditions in the ${location['type']} 
+//         ${location['query']} are ${currentConditions} with a projected high of
+//         ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+//         ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+//         ${forecast['date']}.`;
+
+//         // Resolve the promise with the output text
+//         console.log(output);
+//         resolve(output);
+//       });
+//       res.on('error', (error) => {
+//         console.log(`Error calling the weather API: ${error}`)
+//         reject();
+//       });
+//     });
+//   });
+// }
